b97d5f5a1005c35c841c72f65e8958063d12cb01
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-02-07 11:20:16 +0000

    Drawing the player and having it move

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -5,63 +5,58 @@ import processing.core.PApplet;
 public class HelloProcessing extends PApplet
 {
 
-	public void settings()
+	public void settings() //setting screen size
 	{
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
 		
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+		background(50, 10, 77);  //cool ass colour (in hsb): 200, 50, 100
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
+		fill(237, 237, 50);
+		triangle(250, 50, 40, 470, 460, 470);  //x1, y1, x2, y2, x3, y3
+		
+		fill(250, 250, 250);
+		ellipse(250, 250, 160, 100);  //cx, cy, w, h
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		fill(0, 0 , 0);
+		ellipse(250, 250, 30, 75);
+
+		//fill(0, 0 , 0);
+		triangle(250, 325, 220, 335, 220, 315);
+		triangle(250, 325, 280, 335, 280, 315);
+
+		rectMode(CENTER);
+		rect(250, 50, 60, 10);
+		rectMode(CORNER);
+		rect(235, 0, 30, 50);
+		
+
+		//stroke(255);
+		//line(10, 10, 200, 200);  //x1, y1, x2, y2
+
+
+		// circle(200,250, 50); //cx, cy, r
+		// ellipse(50, 200, 90, 200); //cx, cy, w, h
+		
+		// rectMode(CENTER);  //Ftwo parameters will go from the center of the rectangle
+		// rect(300, 20, 200, 20);   //tlx, tly, w, h
+
+		// rectMode(CORNER);  //FIRST 
+		// rect(300, 20, 200, 20);   //tlx, tly, w, h
+
+		// point(400, 50);  //x, y
+
+		// triangle(10, 400, 50, 450, 300, 200); //x1, y1, x2, y2, x3, y3
 	}
 }

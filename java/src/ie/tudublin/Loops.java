0601ad3d6a929c1f84c52692894d91a09256fa12
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-02-10 10:01:27 +0000

    own magic map function

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -27,15 +27,46 @@ public class Loops extends PApplet
 		println(mode);
 	}
 
-	
+	public float magicMap(float p1,float p2,float p3,float p4,float p5)
+	{
+		float num = p1 - p2;
+		float denom = p3 - p2;
+
+		float ans = (p5 - p4) * (num/denom);
+		return ans;
+		//System.out.println(ans);
+	}
+
+
 	public void draw()
 	{
-		
+		//magicMap(458, 450, 470, 1000, 3000);
 		switch(mode)	
 		{
 			case 0:
+			
+				background(0);
+				int bars = 10;
+				//int bars = mouseX;
+				float w = width / bars;
+				float colorgap = 255 / bars;
+				for(int i = 0; i < bars; i++)
+				{
+					noStroke();
+					fill(magicMap(i, 0, bars, 0, 255), 255, 255);
+					rect(magicMap(i, 0, bars, 0, 500), 0, w, height);
+				}
+
+				// for(float x = 0; x <width; x+=w)  //alternat way of doing the for loop
+				// {
+				// 	rect(x, 0, w, height);
+					
+				// }
+
 				break;
 			case 1:
+
+				//magicMap(458, 450, 470, 1000, 3000);
 				break;
 		}
 	}

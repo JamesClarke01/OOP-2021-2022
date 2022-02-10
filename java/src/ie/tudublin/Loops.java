dcbcb116f4904552c48b4ef748038bc1e32ec35f
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-02-10 09:16:48 +0000

    basic loops

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -6,7 +6,7 @@ public class Loops extends PApplet
 {
 
 
-	int mode = 0;
+	int mode = -1;
 
 	public void settings()
 	{
@@ -27,15 +27,38 @@ public class Loops extends PApplet
 		println(mode);
 	}
 
+	public void rainbowLoop()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			fill((255/10) * i, 255, 255);
+			stroke((255/10) * i, 255, 255);
+			rect(i*width/10, 0, width/10, height);  //x, y, w, h
+		}
+
+	}
+
+	public void rainbowSquares()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			fill((255/10) * i, 255, 255);
+			stroke((255/10) * i, 255, 255);
+			rect(i*width/10, i * height/10, width/10, height/10);
+			rect(width - (i * width/10), i * height/10, -width/10, height/10);
+		}
+	}
+
 	
 	public void draw()
 	{
-		
 		switch(mode)	
 		{
 			case 0:
+				rainbowLoop();
 				break;
 			case 1:
+				rainbowSquares();
 				break;
 		}
 	}

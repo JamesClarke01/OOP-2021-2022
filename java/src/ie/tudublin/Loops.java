a3e60ef969c54f511c3a5f7e71848669b02bbbe3
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-02-17 15:52:23 +0000

    grid and start of star

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -69,6 +69,27 @@ public class Loops extends PApplet
 		line(width/2, height/2, width/2, 50);
 	}
 
+	public void makeCircles()
+	{	
+		//float circles
+		background(255);
+		
+		//float circleDie = width/10;
+		int circles = (int) (mouseX/ 20.0f);
+		float d = width /(float)circles;
+
+
+		for(int i = 0; i < width; i++)
+		{
+			//fill((255/10) * i, 255, 255);
+			//circle(map(i, 0, 10, 0 + circleDie/2, width - circleDie/2), circleDie, circleDie);
+			fill(map(i, 0, circles, 0, 255), 255, 255);
+			circle(map(i, 0, circles - 1, d/2.0f, width - (d/2.0f)), height/2, d);
+
+		}
+
+	}
+
 	
 	public void draw()
 	{
@@ -90,6 +111,49 @@ public class Loops extends PApplet
 				clear();
 				//starThing();
 				break;
+			case 4:
+				clear();
+				makeCircles();
+				break;
+			case 5:
+				background(0);
+				colorMode(RGB);
+				float border = width * 0.1f;
+				for(int i= -5; i <= 5; i++)
+				{
+					float x = map(i, -5, 5, border, width-border);
+					stroke(0, 255, 0);
+					line(x, border, x, height-border);
+					line(border, x, width-border, x);
+					//line(border, x, x, height-border);
+					//line(border, x, x, height-border);
+
+
+				}
+			case 6:
+				float cx = width/2;
+				float cy = height/2;
+
+				float radius = 150;
+				int sides = (int)map(mouseX, 1, width, 0, 20);
+				for(int i = 1; i <= sides; i ++)
+				{
+					float theta1 = map(i - 1, 0, sides, 0, TWO_PI);
+					float x1 = cx +sin(theta1) * radius;
+					float y1 = cy + cos(theta1) * radius;
+
+					float theta2 = map(i, 0, sides, 0, TWO_PI);
+					float x2 = cx + sin(theta2) * radius;
+					float y2 = cy + cos(theta2) * radius;
+					line(x1, y1, x2,y2);
+				}
+
+				line(cx, cy, cx, cy - radius);
+
+
+
+
+
 		}
 	}
 }
